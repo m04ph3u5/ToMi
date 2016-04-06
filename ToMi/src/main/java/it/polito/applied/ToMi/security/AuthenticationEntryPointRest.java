310bf8e428e4038eeecd7e503a840ee2c8c457ff
m04ph3u5 <m04ph3u5@users.noreply.github.com>
2016-04-06 16:48:20 +0200

    Add various security classes

---
+++ ToMi/src/main/java/it/polito/applied/ToMi/security/AuthenticationEntryPointRest.java
@@ -0,0 +1,42 @@
+package it.polito.applied.ToMi.security;
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import it.polito.applied.ToMi.exception.ErrorInfo;
+import it.polito.applied.ToMi.exception.JacksonUtil;
+
+@Component("restServicesEntryPoint")
+public class AuthenticationEntryPointRest extends BasicAuthenticationEntryPoint{
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+			throws IOException, ServletException
+	{
+		String path = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println("COMMENCE: "+path);
+		if(path.startsWith("/assets") || path.startsWith("/api")) {
+			ErrorInfo e = new ErrorInfo();
+			e.setStatusCode(HttpStatus.UNAUTHORIZED.toString());
+			e.setMessage("Devi essere loggato per accedere a questa risorsa");
+			response.addHeader("WWW-Authenticate", "xBasic realm=\"" + getRealmName() + "\"");
+		    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+			String json = JacksonUtil.toJSON(e);
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			response.getWriter().println(json);
+		}else
+		    response.sendRedirect("/404");
+			
+	}
+}
